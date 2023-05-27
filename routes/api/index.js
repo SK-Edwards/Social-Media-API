931ad04fce1f67b7b8fb23d64567507c3947c315
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-27 14:09:29 -0400

    Routes folder and files created and connected.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const thoughtRoutes = require('./thoughtRoutes')
+const userRoutes = require('./userRoutes')
+
+
+router.use('/users', userRoutes);
+
+router.use('/thoughts', thoughtRoutes);
 
 
 
