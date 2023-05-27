799309b0302b2ea9a542ddcf5790c8ff38e754bc
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-27 14:01:48 -0400

    Models virtual added

--- models/thought.js
+++ models/thought.js
@@ -17,7 +17,18 @@ thoughtText: {type: String, require: true, minlength: 1, maxlength: 280},
 createdAt: { type: Date, default: Date.now },
 username: {type: String, required: true},
 reactions: [reactionSchema]
-});
+},
+{
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
