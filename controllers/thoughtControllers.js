41ac706b5a6c19b445f7d59436bb1a18245477e7
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-06-01 10:00:12 -0400

    Working on the thought routes

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -59,25 +59,38 @@ if (!userThought) {
     },
 
     // delete thought 
-
-    async killThought(req, res) {
-        try {
-            const thought = await Thought.findOneAndDelete({_id: req.params.thoughtId});
-            if(!thought) {
-                return res.status(404).json({message: 'You have no thoughts'})
+    
+    killThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+          .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+              return res.status(404).json({ message: 'No thought with this id!' });
             }
-            res.json(thought)
-        } catch (err) {
+            // remove thought id from user's `thoughts` field
+            return User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            );
+          })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              return res.status(404).json({ message: 'Thought created but no user with this id!' });
+            }
+            res.json({ message: 'Thought successfully deleted!' });
+          })
+          .catch((err) => {
+            console.log(err);
             res.status(500).json(err);
-        }
-    },
+          });
+      },
 // creating a reaction
 
     async createReaction(req, res) {
         try {
              const reaction = await Thought.findOneAndUpdate(
                 {_id: req.params.thoughtId},
-                {$push: {reactions: req.body}},
+                {$addToSet: {reactions: req.body}},
                 {new: true}
             );
             if(!reaction) {
@@ -90,18 +103,27 @@ if (!userThought) {
     },
      // delete reactions
 
-     async killReaction(req, res) {
-        try {
-            const reaction = await Thought.findOneAndRemove(
+      killReaction(req, res) {
+        
+            Thought.findOneAndUpdate(
                 {_id: req.params.thoughtId},
-                {$pull: {reactions:{reactionId: req.params.reactionId}}}
-            )
-            if(!reaction) {
-                return res.status(404).json({message: 'No one loves you'})
-            }
-            res.json(reaction)
-        } catch (err) {
-            res.status(500).json(err)
-        }
-     }
+                {$pull: {reactions:{reactionId: req.params.reactionId}}},
+                {runValidators: true, new: true}
+            ).then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    return res.status(404).json({msg: "No thought"})
+                }
+                res.json(dbThoughtData)
+            })
+            .catch((err)=> {
+                console.log(err)
+            })
+            // if(!reaction) {
+            //     return res.status(404).json({message: 'No one loves you'})
+            },
+            // res.json(reaction)
+    //     (err) {
+    //         res.status(500).json(err)
+    //     }
+    //  }
 };
